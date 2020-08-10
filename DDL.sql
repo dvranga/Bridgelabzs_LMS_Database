ffb3443d5b4597bbd396c984837f43c5014831da
RanganathVatti <devangmranganath@gmail.com>
2020-08-10 17:55:23 +0530

    [Ranganath]Added:user details table

--- DDL.sql
+++ DDL.sql
@@ -1,1 +1,12 @@
-create schema `lms_database`;
+CREATE TABLE `lms_database`.`userdetails` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) DEFAULT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  `contact_number` int DEFAULT NULL,
+  `verified` tinyint DEFAULT NULL,
+  `creator_stamp` varchar(45) DEFAULT NULL,
+  `creator_user` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+)
