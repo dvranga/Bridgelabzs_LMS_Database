f2649055bc51b1fa3e53a4453b4c409ab998a41b
RanganathVatti <devangmranganath@gmail.com>
2020-08-10 17:59:28 +0530

    [Ranganath]Added:Hired candidate table

--- DDL.sql
+++ DDL.sql
@@ -1,3 +1,6 @@
+create schema `lms_database`;
+
+
 CREATE TABLE `lms_database`.`userdetails` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(45) DEFAULT NULL,
@@ -8,5 +11,26 @@ CREATE TABLE `lms_database`.`userdetails` (
   `verified` tinyint DEFAULT NULL,
   `creator_stamp` varchar(45) DEFAULT NULL,
   `creator_user` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+);
+
+CREATE TABLE `lms_database`. `hired candidate` (
+  `Id` int NOT NULL,
+  `First Name` varchar(25) DEFAULT NULL,
+  `Middle Name` varchar(25) DEFAULT NULL,
+  `Last Name` varchar(25) DEFAULT NULL,
+  `EmailId` varchar(45) DEFAULT NULL,
+  `Hired City` varchar(45) DEFAULT NULL,
+  `Hired Date` date DEFAULT NULL,
+  `Hired Lab` varchar(45) DEFAULT NULL,
+  `Mobile Number` varchar(45) DEFAULT NULL,
+  `Permanent Pincode` varchar(45) DEFAULT NULL,
+  `Attitude` varchar(45) DEFAULT NULL,
+  `Communication Remark` varchar(45) DEFAULT NULL,
+  `Knowledge Remark` varchar(45) DEFAULT NULL,
+  `Aggregate Remark` varchar(45) DEFAULT NULL,
+  `Status` varchar(45) DEFAULT NULL,
+  `Creator Stamp` varchar(45) DEFAULT NULL,
+  `Creator User` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`Id`)
 )
