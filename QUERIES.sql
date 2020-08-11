744aec8eefbac97774a9a4741f69e93cd89dcb2e
RanganathVatti <devangmranganath@gmail.com>
2020-08-11 18:25:49 +0530

    [Ranganath]Added:(QUERIES)To find the candidate name for those technology is java

--- QUERIES.sql
+++ QUERIES.sql
@@ -1,5 +1,13 @@
 select `First Name`
  from `lms_database`.`fellowship candidate`
+ where `Id`in(select `Id`
+ from `lms_database`.`tech stack`
+ where `tech_name`="java");
+
+
+select `First Name`
+ from `lms_database`.`fellowship candidate`
  where `Id` =(select `Id`
  from `lms_database`.`tech stack`
- where `tech_name` is null)
+ where `tech_name` is null);
+ 
